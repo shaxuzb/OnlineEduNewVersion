5a9074f5e296076fdeec5a6fded10c3d17c0f0f0
shaxuzb <113658910+shaxuzb@users.noreply.github.com>
2026-09-14 15:31:25 +0300

    refactor: add dedicated courses stack route contract

---
+++ src/navigation/coursesTypes.ts
@@ -0,0 +1,9 @@
+export type CoursesStackParamList = {
+  CoursesList: undefined;
+  SubjectScreen: {
+    subjectId: number;
+    subjectName: string;
+    percent: number;
+    subjectCode: string;
+  };
+};
