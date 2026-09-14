153b22e12d93e4ccb10089a7b20fdcb2c22cda20
shaxuzb <113658910+shaxuzb@users.noreply.github.com>
2026-09-14 14:53:13 +0300

    chore: remove unintegrated root navigation helper

--- src/navigation/rootNavigationActions.ts
+++
@@ -1,7 +0,0 @@
-import { CommonActions } from "@react-navigation/native";
-
-export const buildMainTabsResetAction = () =>
-  CommonActions.reset({
-    index: 0,
-    routes: [{ name: "MainTabs" }],
-  });
